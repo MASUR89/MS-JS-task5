bc2417665e68348742f7a3689ab59c00bacfbcf0
MASUR89 <surmava9@gmail.com>
2024-11-17 19:41:58 +0400

    111

--- script.js
+++ script.js
@@ -1,42 +1,67 @@
+let taskCount = 0;
+let completedCount = 0;
+
 function addTask() {
     const taskInput = document.getElementById("taskInput");
     const taskText = taskInput.value.trim();
 
-    if (taskText === "") return;
+    if (taskText === "") {
+        return;
+    }
 
     const taskList = document.getElementById("taskList");
 
     const taskItem = document.createElement("li");
     taskItem.className = "task-item";
+    taskList.appendChild(taskItem);
 
     const taskTextElem = document.createElement("p");
     taskTextElem.className = "task-text";
     taskTextElem.textContent = taskText;
+    taskItem.appendChild(taskTextElem);
 
     const taskButtons = document.createElement("div");
     taskButtons.className = "task-buttons";
+    taskItem.appendChild(taskButtons);
 
     const completeButton = document.createElement("button");
     completeButton.className = "complete-button";
     completeButton.textContent = "Complete";
-    completeButton.onclick = function () {
+    completeButton.addEventListener("click", function () {
         taskItem.classList.toggle("completed");
-    };
+        if (taskItem.classList.contains("completed")) {
+            completedCount++;
+        } else {
+            completedCount--;
+        }
+        updateCounters();
+    });
+    taskButtons.appendChild(completeButton);
 
     const deleteButton = document.createElement("button");
     deleteButton.className = "delete-button";
     deleteButton.textContent = "Delete";
-    deleteButton.onclick = function () {
+    deleteButton.addEventListener("click", function () {
+        if (taskItem.classList.contains("completed")) {
+            completedCount--;
+        }
         taskList.removeChild(taskItem);
-    };
-
-    taskButtons.appendChild(completeButton);
+        taskCount--;
+        updateCounters();
+    });
     taskButtons.appendChild(deleteButton);
 
-    taskItem.appendChild(taskTextElem);
-    taskItem.appendChild(taskButtons);
-
-    taskList.appendChild(taskItem);
-
+    taskCount++;
+    updateCounters();
     taskInput.value = "";
 }
+
+function updateCounters() {
+    const totalTasks = document.querySelector(".totalTasks");
+    totalTasks.textContent = `Total tasks: ${taskCount}`;
+
+    const completedTasks = document.querySelector(".completedTasks");
+    completedTasks.textContent = `Completed tasks: ${completedCount}`;
+}
+
+document.querySelector(".input-section button").addEventListener("click", addTask);
